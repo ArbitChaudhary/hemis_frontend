e4e51e595db04c361d740b1d14d755ff13b61290
ArbitChaudhary <chaudharyarbit45s@gmail.com>
2025-07-01 21:49:14 -0400

    update: env remove

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,18 @@ import router from './router'
 import { CoHome, CoSchool, FaUserGraduate, LaChalkboardTeacherSolid } from 'oh-vue-icons/icons'
 import { addIcons, OhVueIcon } from 'oh-vue-icons'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import { createPinia } from 'pinia'
 
 addIcons(CoHome, CoSchool, FaUserGraduate, LaChalkboardTeacherSolid)
 
 // Register the component globally
 
+const pinia = createPinia()
+
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 app.component('v-icon', OhVueIcon)
 app.use(VueQueryPlugin)
 app.mount('#app')
